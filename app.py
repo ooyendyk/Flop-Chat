c465109e111a3274d4d5793208be53762da0de21
Nerd-1 <ooyendyk@tuta.io>
2019-01-22 15:15:05 +0930

    Removed unused packages

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import os
-import json
-import requests
 
-from flask import Flask, render_template, request, jsonify
-from flask_socketio import SocketIO, emit, send
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
